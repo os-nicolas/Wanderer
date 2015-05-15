6e4fa73bd1a266e3f9cb4067768603fe4b7f8655
os-nicolas <Colin.Wielga@ncf.edu>
2015-05-15 02:02:11 -0400

    sdfg

---
+++ WebSite2/FailedGoal.js
@@ -0,0 +1,37 @@
+﻿function FailedGoal(words, effort,index) {
+    this.words = words;
+    this.effort = effort;
+    this.index = index;
+
+    $('#goal-failed-list').append(
+        "<li id ='goal-failed-" + this.index + "'>" +
+        "<input class='check play' type='checkbox' >"+
+        "<p id ='goal-failed-text"+ this.index+ "'>"+ this.words +"</p>" +
+        "<input id='goal-failed-effort-2' type='number' class='counter' value='"+this.effort+"'>"+
+        "</li>");
+
+    this.toJSON = function () {
+        var out = {}
+        out["words"] = this.words;
+        out["effort"] = this.effort;
+        out["index"] = this.index;
+        return out;
+    }
+
+
+
+    this.getUI = function(){
+        return $("#goal-failed-" + this.index);
+    }
+
+    this.getUI().find('.check').change(function () { g.updateBonus() })
+
+    this.isChecked = function () {
+        return this.getUI().find('.check')[0].checked;
+    }
+
+    
+}
+FailedGoal.fromJSON = function (input) {
+    return new FailedGoal(input["words"], input["effort"], input["index"])
+}
--- WebSite2/goals.js
+++ WebSite2/goals.js
@@ -4,6 +4,9 @@ g.modules.push(Goals);
 
 Goals.count = 3;
 Goals.goals = [];
+Goals.failedGoals = [];
+Goals.passed = 0
+Goals.failed = 0;
 for (var i = 0; i < Goals.count; i++) {
 	Goals.goals.push(new Goal());
 }
@@ -22,7 +25,30 @@ Goals.setEffort = function (value, index) {
 	$("#goal-effort-" + index).val(value);
 }
 
+
+Goals.updatePassed = function(toAdd){
+    Goals.passed += Number(toAdd);
+    $("#goal-passed").text(Goals.passed);
+}
+
+Goals.updateFailed = function (toAdd) {
+    Goals.failed += Number(toAdd);
+    $("#goal-failed").text(Goals.failed);
+}
+
+Goals.setPassed = function (newValue) {
+    Goals.passed = Number(newValue);
+    $("#goal-passed").text(Goals.passed);
+}
+
+Goals.setFailed = function (newValue) {
+    Goals.failed = Number(newValue);
+    $("#goal-failed").text(Goals.failed);
+}
+
 Goals.init = function () {
+    console.log(Goals.goals.length)
+
 	for (var i = 1; i < Goals.count + 1; i++) {
 		$("#goal-edit-" + i).keyup((function(myI){
 			return function () {
@@ -35,31 +61,72 @@ Goals.init = function () {
 			Goals.setEffort($(this).val(), myI);
 			}
 		})(i));
+
+		$("#goal-passed-" + i).click((function (myI) {
+		    return function () {
+		        Goals.updatePassed(Goals.goals[myI - 1].effort);
+		        Goals.setGoal("", myI);
+		        Goals.setEffort(0, myI);
+		    }
+		})(i));
+
+		$("#goal-failed-" + i).click((function (myI) {
+		    return function () {
+		        Goals.updateFailed(Goals.goals[myI - 1].effort);
+		        Goals.failedGoals.push(new FailedGoal(Goals.goals[myI - 1].words, Goals.goals[myI - 1].effort,Goals.failedGoals.length));
+		        Goals.setGoal("", myI);
+		        Goals.setEffort(0, myI);
+		    }
+		})(i));
 	}
 }
 
 Goals.getBonus = function () {
-	return 0;
+    var sum = 0;
+    Goals.failedGoals.forEach(function (failedGoal) {
+        if (failedGoal.isChecked()) {
+            sum++;
+        }
+    })
+    return sum;
 }
 
 Goals.JSONname = "goals";
 
 Goals.toJSON = function (out) {
-	var toOut = []
+    var toOut = {}
+    toOut["goals"] = [];
 	for (var i = 0; i < Goals.count ; i++) {
-		toOut.push(Goals.goals[i].toJSON());
+	    toOut["goals"].push(Goals.goals[i].toJSON());
 	}
+	toOut["failedGoals"] = [];
+	for (var i = 0; i < Goals.failedGoals.length ; i++) {
+	    toOut["failedGoals"].push(Goals.failedGoals[i].toJSON());
+	}
+
+	toOut["passed"] = Goals.passed;
+	toOut["failed"] = Goals.failed;
 	out[Goals.JSONname] = toOut;
 }
 
 Goals.fromJSON = function (input) {
-	if (input[Goals.JSONname] != undefined) {
-		for (var i = 0; i < input[Goals.JSONname].length; i++) {
-			Goals.goals[i] = Goal.fromJSON(input[Goals.JSONname][i]);
-			Goals.setGoal(Goals.goals[i].words, i + 1);
-			Goals.setEffort(Goals.goals[i].effort, i + 1);
-		}
+    if (input[Goals.JSONname] != undefined) {
+        if (input[Goals.JSONname]["goals"] != undefined){
+	        for (var i = 0; i < input[Goals.JSONname]["goals"].length; i++) {
+	            Goals.goals[i] = Goal.fromJSON(input[Goals.JSONname]["goals"][i]);
+			    Goals.setGoal(Goals.goals[i].words, i + 1);
+			    Goals.setEffort(Goals.goals[i].effort, i + 1);
+	        }   
+        }
+
+        if (input[Goals.JSONname]["failedGoals"] != undefined) {
+	        for (var i = 0; i < Goals.failedGoals.length ; i++) {
+	            Goals.failedGoals.push(FailedGoalinput.fromJSON(input[Goals.JSONname]["failedGoals"][i]));
+	        }
+        }
 	}
+    Goals.setPassed(input[Goals.JSONname]["passed"]);
+    Goals.setFailed(input[Goals.JSONname]["failed"]);
 }
 
 Goals.clear = function () {
@@ -68,4 +135,8 @@ Goals.clear = function () {
 		Goals.setGoal("", i + 1);
 		Goals.setEffort(0, i + 1);
 	}
+	$('#goal-failed-list').empty();
+	Goals.failedGoals = [];
+	Goals.passed = 0
+	Goals.failed = 0;
 }
