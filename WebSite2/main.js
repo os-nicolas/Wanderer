c9375e440b50c75c6da3616054baaf469cc03588
os-nicolas <Colin.Wielga@ncf.edu>
2015-11-16 17:28:31 -0500

    better!

--- WebSite2/main.js
+++ WebSite2/main.js
@@ -6,16 +6,16 @@
 function g() { };
 
 g.updateMode = function (write) {
-	g.write = write;
-	$("#all").toggleClass("play");
-	$("#all").toggleClass("write");
-	if (g.write) {
-		$(".item-bonus").prop("disabled", false);
-		$("#mode").text("Done Editing");
-	} else {
-		$(".item-bonus").prop("disabled", true);
-		$("#mode").text("Edit");
-	}
+    g.write = write;
+    $("#all").toggleClass("play");
+    $("#all").toggleClass("write");
+    if (g.write) {
+        $(".item-bonus").prop("disabled", false);
+        $("#mode").text("Done Editing");
+    } else {
+        $(".item-bonus").prop("disabled", true);
+        $("#mode").text("Edit");
+    }
 }
 
 g.none = "-";
@@ -23,8 +23,8 @@ g.character = new Character();
 g.modules = [];
 
 g.updateBonus = function () {
-	var sum = getBonus();
-	$('#totalBonus').text(truncate(sum, 1));
+    var sum = getBonus();
+    $('#totalBonus').text(truncate(sum, 1));
 }
 
 var source;
@@ -41,123 +41,125 @@ function isbefore(a, b) {
 }
 
 function dragenter(e) {
-    console.log("enter!", this)
-    if (isbefore(source, this)) {
-        this.parentNode.insertBefore(source, this);
-    }
-    else {
-        this.parentNode.insertBefore(source, this.nextSibling);
+    //console.log("enter!", this)
+    if (source != this) {
+        if (isbefore(source, this)) {
+            this.parentNode.insertBefore(source, this);
+        }
+        else {
+            this.parentNode.insertBefore(source, this.nextSibling);
+        }
     }
 }
 
 function dragstart(e) {
-    console.log("start!", this)
+    //console.log("start!", this)
     source = this;
     e.originalEvent.dataTransfer.effectAllowed = 'move';
 }
 
 $(document).ready(function () {
 
-	g.updateMode(true)
+    g.updateMode(true)
 
-	SaveLoad.init();
-
-
-	//$("#add-trait").click(function () {
-	//	addElement("trait");
-	//})
+    SaveLoad.init();
 
 
-	$("#roll2").click(Rolls.getRollFunction(2.5));
+    //$("#add-trait").click(function () {
+    //	addElement("trait");
+    //})
 
-	$("#roll5").click(Rolls.getRollFunction(5));
 
-	$("#roll10").click(Rolls.getRollFunction(10));
+    $("#roll2").click(Rolls.getRollFunction(2.5));
 
-	$(".drag").on('dragenter', dragenter);
+    $("#roll5").click(Rolls.getRollFunction(5));
 
-	$(".drag").on('dragstart', dragstart);
+    $("#roll10").click(Rolls.getRollFunction(10));
 
-	//$("#mode").click(function () {
-	//	g.updateMode(!g.write);
-	//});
+    $(".drag").on('dragenter', dragenter);
 
-	$("#misc").change(g.updateBonus);
+    $(".drag").on('dragstart', dragstart);
 
-	$(".hide-button").click(function () {
+    //$("#mode").click(function () {
+    //	g.updateMode(!g.write);
+    //});
+
+    $("#misc").change(g.updateBonus);
+
+    $(".hide-button").click(function () {
         // we go up till we find the section that contains this
-	    var at = $(this);
-	    while (!at.hasClass("section")) {
-	        at = at.parent();
-	    }
-	    // then we 
-	    at.toggleClass("hidden");
-	    if (at.hasClass("hidden")) {
-	        $(this).text("+");
-	    } else {
-	        $(this).text("-");
-	    }
+        var at = $(this);
+        while (!at.hasClass("section")) {
+            at = at.parent();
+        }
+        // then we 
+        at.toggleClass("hidden");
+        if (at.hasClass("hidden")) {
+            $(this).text("+");
+        } else {
+            $(this).text("-");
+        }
 
-	});
+    });
 
-	$(".edit-button").click(function () {
-	    // we go up till we find the section that contains this
-	    var at = $(this);
-	    while (!at.hasClass("section")) {
-	        at = at.parent();
-	    }
-	    // then we 
-	    at.toggleClass("play");
-	    at.toggleClass("write");
-	    if (at.hasClass("play")) {
-	        $(this).text("edit");
-	    } else {
-	        $(this).text("done");
-	    }
+    $(".edit-button").click(function () {
+        // we go up till we find the section that contains this
+        var at = $(this);
+        while (!at.hasClass("section")) {
+            at = at.parent();
+        }
+        // then we 
+        at.toggleClass("play");
+        at.toggleClass("write");
+        if (at.hasClass("play")) {
+            $(this).text("edit");
+        } else {
+            $(this).text("done");
+        }
 
-	});
+    });
 
-	var buttons = $(".edit-button");
-	console.log("buttons", buttons);
-	buttons.each(function () {
-	    var at = $(this);
-	    while (!at.hasClass("section")) {
-	        at = at.parent();
-	    }
-	    if (at.hasClass("play")) {
-	        $(this).text("edit");
-	    } else {
-	        $(this).text("done");
-	    }
-	});
+    var buttons = $(".edit-button");
+    console.log("buttons", buttons);
+    buttons.each(function () {
+        var at = $(this);
+        while (!at.hasClass("section")) {
+            at = at.parent();
+        }
+        if (at.hasClass("play")) {
+            $(this).text("edit");
+        } else {
+            $(this).text("done");
+        }
+    });
 
-	g.modules.forEach(function (mod) {
-		mod.init();
-	});
+    g.modules.forEach(function (mod) {
+        mod.init();
+    });
 });
 
 function truncate(num, order) {
-	if (order == undefined) {
-		order = 0;
-	}
-	var by = Math.pow(10, order)
+    if (order == undefined) {
+        order = 0;
+    }
+    var by = Math.pow(10, order)
 
-	return Math.floor(num * by) / by;
+    return Math.floor(num * by) / by;
 }
 
 
 function getBonus() {
-	var sum = 0;
-	sum += Number($("#misc").val());
+    var sum = 0;
+    sum += Number($("#misc").val());
 
-	g.modules.forEach(function (mod) {
-		sum += mod.getBonus();
-	});
+    g.modules.forEach(function (mod) {
+        sum += mod.getBonus();
+    });
 
-	return sum;
+    return sum;
 }
 
 //TODO move this in to skill
 function toId(skillName) {
-	return skillName.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '').toLowerCase();
+    return skillName.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '').toLowerCase();
 }
